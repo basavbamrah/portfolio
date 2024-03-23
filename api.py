b357364fa6da73bf8f1fe59ec455485066e184c4
basavbamrah <basavbamrah@gmail.com>
2024-03-23 17:03:25 +0530

    initial-flask

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask 
+from flask_cors import CORS
+import os
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
