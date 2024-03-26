879be3ede74cc4d52212f8b90a938f6c2db4a49a
basavbamrah <basavbamrah@gmail.com>
2024-03-26 21:42:07 +0530

    requirements.txt

--- api.py
+++ api.py
@@ -1,18 +1,34 @@
-from flask import Flask 
+from flask import Flask, request
 from flask_cors import CORS
+from firebase_admin import credentials, firestore, storage
+
 import os
 
 
-
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["*"])
+
+# cred_file = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+# cred = credentials.Certificate(cred_file)
+# admin = firebase_admin.initialize_app(credential=cred)
+# db = firestore.client()
+
+
+@app.route("/", methods=["GET"])
+def index():
+    if request.method == "GET":
+        return {
+            'status': "success",
+            'version': "0.0.1"
+        }, 200
+    else:
+        return {
+            'status': "error",
+            'message': "Method not allowed"
+        }, 405
 
 
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
